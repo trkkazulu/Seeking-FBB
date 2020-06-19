297337ef6bdd5aea4e5f24d798c67c3e00fd4b0b
trkkazulu <jairrohm@gmail.com>
2020-06-19 19:36:14 -0400

    Working out the code for importing json data

--- Seeking FBB/Controllers/DetailViewController.swift
+++ Seeking FBB/Controllers/DetailViewController.swift
@@ -22,18 +22,50 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let jsonData: Data = "AthletData.json"
-//        let jsonDict = try JSONSerialization.jsonObject(with: jsonData) as? NSDictionary
+        var athData: Data!
         
-        let athArray: [AthleteModel] = load("AthleteData.json")
-
+        //let jsonData: Data = athArray
+        //let jsonDict = try JSONSerialization.jsonObject(with: jsonData) as? NSDictionary
         
         lbl.text = name
         img.image = image
-        //infoLbl.text = id
+        // infoLbl.text = print(athArray)
         
-        print(athArray)
+        athData = readLocalFile(forName: "AthleteData")
         
+        print(athData)
+        
+       // parse(jasonData: athData)
+        
+  
+    }
+    
+    private func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name,
+                                                 ofType: "json"),
+                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+
+    
+    func parse(jasonData: Data) {
+        do {
+            let decodedData = try JSONDecoder().decode(AthleteModel.self,
+                                                       from: jasonData)
+            
+            print("Name: ", decodedData.name)
+            print("Date of Birth: ", decodedData.dateOfBirth)
+            print("===================================")
+        } catch {
+            print("decode error")
+        }
     }
     
     func load<T: Decodable>(_ filename: String) -> T {
@@ -57,7 +89,7 @@ class DetailViewController: UIViewController {
             fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
         }
     }
-
+    
     
     @IBAction func voteBtnPressed(_ sender: Any) {
     }
